30e9d9b096d0e655049e64aada309a1f0ba71c2e
eetann <hideharu092@gmail.com>
2021-11-18 18:12:37 +0900

    MarkdownにChakraUIを適用

--- src/components/modal-md.tsx
+++ src/components/modal-md.tsx
@@ -1,8 +1,33 @@
+import {Link, ListItem, OrderedList, Text, UnorderedList} from "@chakra-ui/react";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+
 
 type Props = {body: string};
 
 export const ModalMarkdown: React.FC<Props> = ({body}) => {
-  return <ReactMarkdown children={body} />;
+  return <ReactMarkdown children={body}
+    components={{
+      text: ({children}) => {
+        return <Text>{children}</Text>
+      },
+      p: ({children}) => {
+        return <Text mt="4">{children}</Text>
+      },
+      ul: ({children}) => {
+        return <UnorderedList my="2">{children}</UnorderedList>
+      },
+      ol: ({children}) => {
+        return <OrderedList>
+          {children}
+        </OrderedList>
+      },
+      li: ({children}) => {
+        return <ListItem py="1">{children}</ListItem>
+      },
+      a: ({children, href}) => {
+        return <Link href={href} color="teal.500">{children}</Link>;
+      },
+    }}
+  />;
 }

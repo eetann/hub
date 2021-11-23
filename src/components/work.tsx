27c46a43460a8a6de2f61dcd888a61029b53436d
eetann <hideharu092@gmail.com>
2021-11-23 14:35:51 +0900

    disclosureを関数コンポーネント化

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -32,86 +32,90 @@ interface ContentScheme extends MicroCMSListContent {
 
 export type WorkContent = ContentScheme & MicroCMSContentId & MicroCMSDate;
 
+const OneWork = ({work}: {work: WorkContent}) => {
+  const {isOpen, onOpen, onClose} = useDisclosure();
+  return (
+    <Fragment key={work.id} >
+      <WrapItem bg="white"
+        p="4"
+        rounded="4"
+        boxShadow="base"
+        _hover={{boxShadow: "outline"}}
+        onClick={onOpen}
+        cursor="pointer"
+      >
+        <Box>
+          <Box width="300px" height="250px">
+            {typeof work.image !== "undefined" ?
+              <Image
+                src={work.image.url}
+                alt={work.name}
+                width="300px"
+                height="220px"
+                objectFit="contain"
+              />
+              :
+              <Center bgColor="cyan.500"
+                fontSize="50px" color="white" fontWeight="semibold"
+                height="220px"
+              >
+                (*&apos;u&apos;*)ノ
+              </Center>
+            }
+          </Box>
+          <Box as="h4"
+            mt="1"
+            fontWeight="semibold"
+            isTruncated
+          >{work.name}</Box>
+        </Box>
+      </WrapItem>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{work.name}</ModalHeader>
+          <ModalCloseButton />
+          {typeof work.body !== "undefined" ?
+            <ModalBody>
+              {typeof work.image !== "undefined" ?
+                <Center>
+                  <Image
+                    src={work.image.url}
+                    alt={work.name}
+                    width={450}
+                    height={300}
+                    objectFit="contain"
+                  /></Center> : <></>}
+              <ModalMarkdown body={work.body} />
+            </ModalBody>
+            : <></>}
+          <ModalFooter>
+            <Link href={work.url} isExternal mx="auto" fontSize="lg" fontWeight="semibold" color="teal.500">
+              Here!
+              <ExternalLinkIcon mx="2px" />
+            </Link>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Fragment>
+  )
+
+};
+
 export function Work({works}: {works: WorkContent[]}) {
   return (
     <Wrap spacing={5} align="center">
       {works.map((work) => {
-        const {isOpen, onOpen, onClose} = useDisclosure();
-        return (
-          <Fragment key={work.id} >
-            <WrapItem bg="white"
-              p="4"
-              rounded="4"
-              boxShadow="base"
-              _hover={{boxShadow: "outline"}}
-              onClick={onOpen}
-              cursor="pointer"
-            >
-              <Box>
-                <Box width="300px" height="250px">
-                  {typeof work.image !== "undefined" ?
-                    <Image
-                      src={work.image.url}
-                      alt={work.name}
-                      width="300px"
-                      height="220px"
-                      objectFit="contain"
-                    />
-                    :
-                    <Center bgColor="cyan.500"
-                      fontSize="50px" color="white" fontWeight="semibold"
-                      height="220px"
-                    >
-                      (*'u'*)ノ
-                    </Center>
-                  }
-                </Box>
-                <Box as="h4"
-                  mt="1"
-                  fontWeight="semibold"
-                  isTruncated
-                >{work.name}</Box>
-              </Box>
-            </WrapItem>
-            <Modal isOpen={isOpen} onClose={onClose} size="xl">
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>{work.name}</ModalHeader>
-                <ModalCloseButton />
-                {typeof work.body !== "undefined" ?
-                  <ModalBody>
-                    {typeof work.image !== "undefined" ?
-                      <Center>
-                        <Image
-                          src={work.image.url}
-                          alt={work.name}
-                          width={450}
-                          height={300}
-                          objectFit="contain"
-                        /></Center> : <></>}
-                    <ModalMarkdown body={work.body} />
-                  </ModalBody>
-                  : <></>}
-                <ModalFooter>
-                  <Link href={work.url} isExternal mx="auto" fontSize="lg" fontWeight="semibold" color="teal.500">
-                    Here!
-                    <ExternalLinkIcon mx="2px" />
-                  </Link>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
-          </Fragment>
-        )
+        <OneWork key={work.id} work={work} />
       })}
     </Wrap>
   )
 }
 
-export const getWorks = async (client: MicroCMSClientObj, _draftKey: string): Promise<WorkContent[]> => {
+export const getWorks = async (client: MicroCMSClientObj): Promise<WorkContent[]> => {
   const data = await client.get<MicroCMSListResponse<ContentScheme>>(
     {
       endpoint: "work",
-      queries: {draftKey: _draftKey},
     }
   );
   return data.contents;

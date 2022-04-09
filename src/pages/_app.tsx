ae41ad67cd2fb8c0fa69c544400353916d5abd21
eetann <hideharu092@gmail.com>
2022-04-09 20:41:45 +0900

    change title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import {ChakraProvider} from "@chakra-ui/react";
-import theme from "../theme"
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
 
-function MyApp({Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>eetann</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
